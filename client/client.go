059092b51d6fdbfe4e17d99d2aa3052818f68344
xtoter <misha37a999@yandex.ru>
2021-10-31 03:57:42 +0300

    Докрутил web-сокеты
    Все работает,вроде

--- client/client.go
+++ client/client.go
@@ -1,46 +1,173 @@
+//go:build ignore
+// +build ignore
+
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"net"
+	"html/template"
 	"net/http"
+	"net/url"
 	"os"
+	"os/signal"
 
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 )
 
-func read(conn net.Conn) {
+var upgrader = websocket.Upgrader{} // use default options
+var addr = flag.String("addr", "localhost:8080", "http service address")
+var addrweb = flag.String("addrweb", "localhost:9090", "http service address")
+var server *websocket.Conn
+
+func read(c, d *websocket.Conn) {
+	done := make(chan struct{})
+	defer close(done)
 	for {
-		message, err := bufio.NewReader(conn).ReadString('\n')
-		if err == nil {
-			fmt.Print(message)
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			return
 		}
-	}
-
-}
-func main2() {
-
-	// Подключаемся к сокету
-	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-	go read(conn)
-	for {
-		// Чтение входных данных от stdin
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		// Отправляем в socket
-		fmt.Fprintf(conn, text+"\n")
-		// Прослушиваем ответ
+		err = d.WriteMessage(websocket.TextMessage, message)
+		//fmt.Printf("recv: %s\n", message)
 	}
 }
 func main() {
-	http.Handle("/", websocket.Handler(handler))
-	http.ListenAndServe("localhost:3000", nil)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	fmt.Printf("connecting to %s\n", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println("dial:", err)
+	}
+	defer c.Close()
+	//go read(c)
+	server = c
+	//
+
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/", home)
+	http.ListenAndServe(*addrweb, nil)
+	for {
+
+	}
+}
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			break
+		}
+		go read(server, c)
+		server.WriteMessage(websocket.TextMessage, []byte(message))
+
+		if err != nil {
+			fmt.Println("write:", err)
+			break
+		}
+	}
 }
 
-func handler(c *websocket.Conn) {
-	var s string
-	fmt.Fscan(c, &s)
-	fmt.Println("Received:", s)
-	fmt.Fprint(c, "How do you do?")
+func home(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
 }
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+	var number = document.getElementById("number");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print(evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("ping").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("ping "+input.value+" "+number.value);
+        ws.send("ping,"+input.value+","+number.value);
+        return false;
+    };
+	document.getElementById("trace").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("trace "+input.value);
+        ws.send("trace,"+input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="ya.ru">
+<input id="number" type="number" name="t" value="2" min="0" max="9999" step="1">
+<button id="ping">Ping</button>
+<button id="trace">Trace</button>
+</form>
+
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td><td valign="top" width="50%">
+</td></tr></table>
+</body>
+</html>
+`))
--- server/server.go
+++ server/server.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"net"
+	"log"
+	"net/http"
 	"os/exec"
 	"strconv"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/sparrc/go-ping"
 )
 
-func getping(conn net.Conn, host string, count int) {
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+func getping(c *websocket.Conn, host string, count int) {
 	pinger, err := ping.NewPinger(host)
 	pinger.Count = count
 	if err != nil {
-		conn.Write([]byte("Error: " + err.Error()))
+		c.WriteMessage(websocket.TextMessage, []byte("Error: "+err.Error()))
 		//fmt.Printf("ERROR: %s\n", err.Error())
 		return
 	}
@@ -24,34 +28,34 @@ func getping(conn net.Conn, host string, count int) {
 		out := fmt.Sprintf("%d bytes from %s: icmp_seq=%d time=%v\n",
 
 			pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
-		conn.Write([]byte(out))
+		c.WriteMessage(websocket.TextMessage, []byte(out))
 		//fmt.Println(pkt)
 	}
 
 	pinger.OnFinish = func(stats *ping.Statistics) {
 		out := fmt.Sprintf("\n--- %s ping statistics ---\n", stats.Addr)
-		conn.Write([]byte(out))
+		c.WriteMessage(websocket.TextMessage, []byte(out))
 		time.Sleep(100 * time.Millisecond)
 		out = fmt.Sprintf("%d packets transmitted, %d packets received, %v%% packet loss\n",
 			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-		conn.Write([]byte(out))
+		c.WriteMessage(websocket.TextMessage, []byte(out))
 		time.Sleep(100 * time.Millisecond)
 
 		out = fmt.Sprintf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
 			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
-		conn.Write([]byte(out))
+		c.WriteMessage(websocket.TextMessage, []byte(out))
 		time.Sleep(100 * time.Millisecond)
 
 	}
 	out := fmt.Sprintf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
-	conn.Write([]byte(out))
+	c.WriteMessage(websocket.TextMessage, []byte(out))
 	time.Sleep(100 * time.Millisecond)
 
 	pinger.Run()
 	//conn.Write([]byte("\n"))
 
 }
-func gettrace(conn net.Conn, host string) {
+func gettrace(c *websocket.Conn, host string) {
 	dateCmd := exec.Command("traceroute", host)
 	dateOut, err := dateCmd.Output()
 	if err != nil {
@@ -61,7 +65,7 @@ func gettrace(conn net.Conn, host string) {
 	for i := 0; i < len(dateOut); i++ {
 		if dateOut[i] == byte(10) {
 			//fmt.Println(string(dateOut[start:i]))
-			conn.Write([]byte(string(dateOut[start:i]) + "\n"))
+			c.WriteMessage(websocket.TextMessage, []byte(string(dateOut[start:i])+"\n"))
 			start = i + 1
 			time.Sleep(100 * time.Millisecond)
 		}
@@ -72,7 +76,7 @@ func gettrace(conn net.Conn, host string) {
 func decode(str string) (mode, host string, count int) {
 	var num []int
 	num = append(num, len(str)-1)
-	num = append(num, len(str)-1)
+	num = append(num, -1)
 	j := 0
 	for i := 0; i < len(str); i++ {
 		if str[i] == byte(44) {
@@ -82,44 +86,52 @@ func decode(str string) (mode, host string, count int) {
 
 	}
 	count = 0
-	if num[1] != len(str)-1 {
-		count, _ = strconv.Atoi(str[(num[1] + 1) : len(str)-1])
+	if num[1] != -1 {
+		count, _ = strconv.Atoi(str[(num[1] + 1):])
+	} else {
+		num[1] = len(str)
 	}
 
 	return str[0:num[0]], str[(num[0] + 1):num[1]], count
 }
-func client(conn net.Conn) {
+func client(c *websocket.Conn) {
 	for {
-		// Будем прослушивать все сообщения разделенные \n
-		message, err1 := bufio.NewReader(conn).ReadString('\n')
-		if err1 != nil {
-			fmt.Println("error")
-			return
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
 		}
+		fmt.Println(string(message))
+
 		// Распечатываем полученое сообщение
-		fmt.Print(string(message))
 		// Процесс выборки для полученной строки
 		// Отправить новую строку обратно клиенту
 
-		mode, host, count := decode(message)
+		mode, host, count := decode(string(message))
 		switch mode {
 
 		case "ping":
-			go getping(conn, host, count)
+			go getping(c, host, count)
 		case "trace":
-			go gettrace(conn, host)
+			go gettrace(c, host)
 		}
 
 	}
 }
+
+var upgrader = websocket.Upgrader{} // use default options
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	client(c)
+}
 func main() {
 	fmt.Println("Launching server...")
+	http.HandleFunc("/echo", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 
-	ln, _ := net.Listen("tcp", ":8081")
-	for {
-		conn, err := ln.Accept()
-		if err == nil {
-			go client(conn)
-		}
-	}
 }
